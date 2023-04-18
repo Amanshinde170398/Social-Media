cee4badb48e6b5c32b2f76e1a7bdebde9b40ad72
Aman Shinde <1703amanshinde98@gmail.com>
2023-04-18 22:20:17 +0530

    project setup with views and layouts

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require("express");
+
 const db = require("./config/moongose");
 const User = require("./models/user");
-
+const expressLayouts = require("express-ejs-layouts");
 const PORT = 8080;
 
 const app = express();
 
 app.use(express.urlencoded());
+app.use(expressLayouts);
 app.use("/", require("./routes"));
 app.use(express.static("assets"));
+
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
